11014c1411043121ef72c6ee9013fa4892f05175
verbavolant <marco.manenti@gmail.com>
2020-04-19 19:59:44 +0200

    Some updates, bug in spaced namefile

--- reMarkableSync.sh
+++ reMarkableSync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# v1.2
 
 if [[ -e "/tmp/reMarkableSync.pid" ]] ; then
     echo "another is running, exit"
@@ -6,20 +7,22 @@ else
 
 touch "/tmp/reMarkableSync.pid"
 
-cartella="/home/mm/remarkable/Sincronizza"
+cartella="/home/mm/reMarkable-autosync/Sincronizza"
 
-cd /home/mm/remarkable/Sincronizza
+cd /home/mm/reMarkable-autosync/Sincronizza
 
-rclone move nextcloud:/invia_a_marco/ /home/mm/remarkable/Sincronizza/
+rclone move nextcloud:/invia_a_marco/ /home/mm/reMarkable-autosync/Sincronizza/
+
+for f in *\ *; do mv "$f" "${f// /_}" 2> /dev/null; done
 
 mv *.PDF *.pdf 2> /dev/null
 mv *.ZIP *.zip 2> /dev/null
 
-for filename in /home/mm/remarkable/Sincronizza/*.zip; do
+for filename in /home/mm/reMarkable-autosync/Sincronizza/*.zip; do
 
     [ -f "$filename" ] || continue
     basenamepdf="${filename##*/}"
-    echo "elaboro il file $basenamepdf $filename"
+    echo "elaboro il file $basenamepdf $filename" >> /tmp/reMarkable.log
     unzip -o "$filename"
     rm "$filename"
 
@@ -27,19 +30,19 @@ done
 
 cd /home/mm/remarkable
 
-for filename in /home/mm/remarkable/Sincronizza/*.pdf; do
+for filename in /home/mm/reMarkable-autosync/Sincronizza/*.pdf; do
 
     [ -f "$filename" ] || continue
     basenamepdf="${filename##*/}"
-    echo "elaboro il file $basenamepdf $filename"
-    php /home/mm/remarkable/remarkable.php upload "Sincronizza/$basenamepdf" Upload
+    echo "elaboro il file $basenamepdf $filename"  >> /tmp/reMarkable.log
+    php /home/mm/reMarkable-autosync/remarkable.php upload "Sincronizza/$basenamepdf" Upload
     sleep 5
     rm "Sincronizza/$basenamepdf"
     echo "$filename" >> /tmp/sincronizzati
 
 done
 
-rclone move /home/mm/remarkable/Sincronizza/ nextcloud:/scartati_da_marco/
+rclone move /home/mm/reMarkable-autosync/Sincronizza/ nextcloud:/scartati_da_marco/
 
 rm "/tmp/reMarkableSync.pid"
 
