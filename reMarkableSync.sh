c1fd98e9a2e802752413770122d8b78e0d16391c
Marco Manenti <marco.manenti@gmail.com>
2023-02-13 14:48:53 +0100

    Update reMarkableSync.sh
    little bugfix

--- reMarkableSync.sh
+++ reMarkableSync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v1.3
+# v1.4
 
 if [[ -e "/tmp/reMarkableSync.pid" ]] ; then
     echo "another is running, exit"
@@ -7,11 +7,13 @@ else
 
 touch "/tmp/reMarkableSync.pid"
 
-cartella="/home/mm/reMarkable-autosync/Sincronizza"
+cartella="/home/marco/reMarkable-autosync/Sincronizza"
 
-cd /home/mm/reMarkable-autosync/Sincronizza
+cd /home/marco/reMarkable-autosync/Sincronizza
 
-rclone move nextcloud:/invia_a_marco/ /home/mm/reMarkable-autosync/Sincronizza/
+rclone move nextcloud:/invia_a_marco/ /home/marco/reMarkable-autosync/Sincronizza/
+#add another sync point
+#rclone move xxxx:xxxx /home/marco/reMarkable-autosync/Sincronizza/
 
 for f in *\ *; do mv "$f" "${f// /_}" 2> /dev/null; done
 
@@ -19,18 +21,17 @@ for f in *; do
     test -f "$f" && mv "$f" "${f,,}"
 done
 
-mv *.PDF *.pdf 2> /dev/null
-mv *.ZIP *.zip 2> /dev/null
-
-for filename in /home/mm/reMarkable-autosync/Sincronizza/*.zip; do
+for filename in /home/marco/reMarkable-autosync/Sincronizza/*.zip; do
 
     [ -f "$filename" ] || continue
     basenamepdf="${filename##*/}"
     echo "elaboro il file $basenamepdf $filename" >> /tmp/reMarkable.log
     unzip -o "$filename"
     rm "$filename"
-
 done
+
+mv *.PDF *.pdf 2> /dev/null
+mv *.ZIP *.zip 2> /dev/null
 
 cd /home/mm/reMarkable-autosync/
 test -f *.pdf && /home/mm/reMarkable-autosync/rmapi mkdir Upload/"$(date +"%d-%m-%Y")"
@@ -39,14 +40,15 @@ for filename in /home/mm/reMarkable-autosync/Sincronizza/*.pdf; do
 
     [ -f "$filename" ] || continue
     basenamepdf="${filename##*/}"
-    echo "elaboro il file $basenamepdf $filename"  >> /tmp/reMarkable.log
-
+    /home/marco/reMarkable-autosync/rmapi mkdir Upload/"$(date +"%Y-%m-%d")"
+    echo "elaboro il file $(date +"%Y-%m-%d") $basenamepdf $filename"  >> /tmp/reMarkable.log
+    
 ######################
 #uncomment for remarkable api
-#    php /home/mm/reMarkable-autosync/remarkable.php upload "Sincronizza/$basenamepdf" Upload
+#    php /home/marco/reMarkable-autosync/remarkable.php upload "Sincronizza/$basenamepdf" Upload
 
 #uncomment for rmapi
-    /home/mm/go/bin/rmapi put "Sincronizza/$basenamepdf" Upload
+    /home/marco/reMarkable-autosync/rmapi put "Sincronizza/$basenamepdf" Upload/"$(date +"%Y-%m-%d")"  >> /tmp/rmapi.log
 ######################
 
     sleep 5
